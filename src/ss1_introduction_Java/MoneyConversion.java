9092107f57345bb439d44e29984a48a67c1b0012
soljinn <soljin97@gmail.com>
2022-12-30 11:01:28 +0700

    first commit

---
+++ src/ss1_introduction_Java/MoneyConversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MoneyConversion {
+    public static void main(String[] args) {
+        double Usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input money USD");
+        Usd = scanner.nextDouble();
+        double Vnd = Usd*23000;
+        System.out.println("Value VND: "+Vnd);
+    }
+}
