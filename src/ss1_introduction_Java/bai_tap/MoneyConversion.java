06075d70cfe190607c7ed1b05c67466eae9b478c
soljinn <soljin97@gmail.com>
2023-01-03 11:26:27 +0700

    update

--- src/ss1_introduction_Java/bai_tap/MoneyConversion.java
+++ src/ss1_introduction_Java/bai_tap/MoneyConversion.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MoneyConversion {
     public static void main(String[] args) {
-        double Usd;
+        double usd;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input money USD");
-        Usd = scanner.nextDouble();
-        double Vnd = Usd*23000;
-        System.out.println("Value VND: "+Vnd);
+        usd = Double.parseDouble(scanner.nextLine());
+        double Vnd = usd * 23000;
+        System.out.println("Value VND: " + Vnd);
     }
 }
