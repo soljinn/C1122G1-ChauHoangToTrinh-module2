956619c070eebca43e072936cf5baa1c36f2f07a
soljinn <soljin97@gmail.com>
2023-01-09 09:45:37 +0700

    update

--- src/ss5_access_modifier/bai_tap/circle/MainCircle.java
+++ src/ss5_access_modifier/bai_tap/circle/MainCircle.java
@@ -2,8 +2,7 @@ package ss5_access_modifier.bai_tap.circle;
 
 import java.util.Scanner;
 
-public class
-MainCircle {
+public class MainCircle {
     public static void main(String[] args) {
         Circle circle = new Circle();
         System.out.println("Circle 1: \n" + circle);
