3fc27b4501ba67e619732e6edcb49377c19c6983
soljinn <soljin97@gmail.com>
2023-01-12 11:44:51 +0700

    update

--- src/ss8_clean_code/TennisGame.java
+++ src/ss8_clean_code/TennisGame.java
@@ -1,11 +1,11 @@
 package ss8_clean_code;
 
 public class TennisGame {
-    public static String getScore(String player1Name, String player2Name, int mScore1, int mScore2) {
+    public static String getScore(String player1Name, String player2Name, int score1, int score2) {
         String score = "";
         int tempScore;
-        if (mScore1 == mScore2) {
-            score = switch (mScore1) {
+        if (score1 == score2) {
+            score = switch (score1) {
                 case 0 -> "Love-All";
                 case 1 -> "Fifteen-All";
                 case 2 -> "Thirty-All";
@@ -13,8 +13,8 @@ public class TennisGame {
                 default -> "Deuce";
             };
             return score;
-        } else if (mScore1 >= 4 || mScore2 >= 4) {
-            int minusResult = mScore1 - mScore2;
+        } else if (score1 >= 4 || score2 >= 4) {
+            int minusResult = score1 - score2;
             if (minusResult == 1) {
                 score = "Advantage player1";
             } else if (minusResult == -1) {
@@ -27,10 +27,10 @@ public class TennisGame {
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) {
-                    tempScore = mScore1;
+                    tempScore = score1;
                 } else {
                     score += "-";
-                    tempScore = mScore2;
+                    tempScore = score2;
                 }
                 score = getString(score, tempScore);
             }
